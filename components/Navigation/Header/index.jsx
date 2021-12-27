0423bf7542f67178d73a3a0b448519c900a71bee
Salik Imtiaz <r.salikimtiaz@gmail.com>
2021-12-27 18:49:21 +0500

    made header fixed to top

--- components/Navigation/Header/index.jsx
+++ components/Navigation/Header/index.jsx
@@ -15,8 +15,8 @@ function Header() {
         window.addEventListener('scroll', changeBackground);
     }, []);
     return (
-        <div className={`sticky left-0 top-0 xs:px-6 lg:px-20 py-2 z-50  ${navbar ? 'shadow-xl bg-white' : ''}`}>
-            <h1 className='text-24 font-semibold text-primary'>Salik <span className='text-black font-light'>Imtiaz</span></h1>
+        <div className={`fixed xs:px-6 lg:px-20 py-2 z-50  ${navbar ? 'shadow-xl' : ''}`}>
+            <h1 className='text-24 font-semibold text-primary uppercase'>Salik <span className='text-black font-light'>Imtiaz</span></h1>
         </div>
     )
 }
