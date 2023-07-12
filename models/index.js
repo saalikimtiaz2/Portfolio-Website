3662cfc880684bdaefbda0a52ae1002d940fde6c
Salik Imtiaz <r.salikimtiaz@gmail.com>
2023-07-12 15:02:32 +0400

    edited content and added coffee icon

--- models/index.js
+++ models/index.js
@@ -1,47 +1,62 @@
 export const icons = [
-  {
-    image: '/assets/icons/html.svg',
-    darkImage: '/assets/icons/dark-html.svg',
-    name: 'HTML',
-  },
-  {
-    image: '/assets/icons/css.svg',
-    darkImage: '/assets/icons/dark-css.svg',
-    name: 'CSS',
-  },
-  {
-    image: '/assets/icons/sass.svg',
-    darkImage: '/assets/icons/dark-sass.svg',
-    name: 'Sass',
-  },
-  {
-    image: '/assets/icons/javascript.svg',
-    darkImage: '/assets/icons/dark-js.svg',
-    name: 'Javascript',
-  },
-  {
-    image: '/assets/icons/typescript.svg',
-    darkImage: '/assets/icons/dark-ts.svg',
-    name: 'Typescript',
-  },
-  {
-    image: '/assets/icons/react.svg',
-    darkImage: '/assets/icons/dark-react.svg',
-    name: 'React Js',
-  },
-  {
-    image: '/assets/icons/next.svg',
-    darkImage: '/assets/icons/dark-next.svg',
-    name: 'Next Js',
-  },
-  {
-    image: '/assets/icons/redux.svg',
-    darkImage: '/assets/icons/dark-redux.svg',
-    name: 'Redux',
-  },
-  {
-    image: '/assets/icons/jest.svg',
-    darkImage: '/assets/icons/dark-jest.svg',
-    name: 'Jest',
-  },
+	{
+		image: '/assets/icons/html.svg',
+		darkImage: '/assets/icons/dark-html.svg',
+		name: 'HTML',
+	},
+	{
+		image: '/assets/icons/css.svg',
+		darkImage: '/assets/icons/dark-css.svg',
+		name: 'CSS',
+	},
+	{
+		image: '/assets/icons/sass.svg',
+		darkImage: '/assets/icons/dark-sass.svg',
+		name: 'Sass',
+	},
+	{
+		image: '/assets/icons/javascript.svg',
+		darkImage: '/assets/icons/dark-js.svg',
+		name: 'Javascript',
+	},
+	{
+		image: '/assets/icons/typescript.svg',
+		darkImage: '/assets/icons/dark-ts.svg',
+		name: 'Typescript',
+	},
+	{
+		image: '/assets/icons/react.svg',
+		darkImage: '/assets/icons/dark-react.svg',
+		name: 'React Js',
+	},
+	{
+		image: '/assets/icons/next.svg',
+		darkImage: '/assets/icons/dark-next.svg',
+		name: 'Next Js',
+	},
+	{
+		image: '/assets/icons/nodejs.svg',
+		darkImage: '/assets/icons/dark-nodejs.svg',
+		name: 'Node Js',
+	},
+	{
+		image: '/assets/icons/express.svg',
+		darkImage: '/assets/icons/dark-express.svg',
+		name: 'Express Js',
+	},
+	{
+		image: '/assets/icons/mongo.svg',
+		darkImage: '/assets/icons/dark-mongo.svg',
+		name: 'mongoDB',
+	},
+	{
+		image: '/assets/icons/redux.svg',
+		darkImage: '/assets/icons/dark-redux.svg',
+		name: 'Redux',
+	},
+	{
+		image: '/assets/icons/jest.svg',
+		darkImage: '/assets/icons/dark-jest.svg',
+		name: 'Jest',
+	},
 ];
