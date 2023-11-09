2270cf0d28c9cc5dee34f123ca79983d49c3aa38
Salik Imtiaz <r.salikimtiaz@gmail.com>
2023-11-10 00:14:34 +0400

    removed un-used hook

--- src/hooks/Sanity/useFetchSanity.ts
+++
@@ -1,9 +0,0 @@
-import { ProfileType } from "@/interfaces/sanity";
-
-const useFetchSanity = async (func: () => {}): [ProfileType] => {
-  const data: {} = await func();
-
-  return [data];
-};
-
-export default useFetchSanity;
