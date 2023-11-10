959c0860b37a9756df26c42c5e2885a0ea1885a8
Salik Imtiaz <r.salikimtiaz@gmail.com>
2023-11-10 16:15:07 +0400

    Card desgin changed

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,5 @@
 import { projectTypes } from "@/interfaces/sanity";
+import hexRgb from "hex-rgb";
 import { redirect } from "next/navigation";
 import { useState } from "react";
 import { BsArrowRight } from "react-icons/bs";
@@ -7,34 +8,37 @@ const Card = ({ project }: { project: projectTypes }) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <div
-      className="card relative rounded-2xl hover:shadow-2xl transition-all duration-500 ease-in-out hover:-translate-y-4 mb-[10rem]"
-      style={{ backgroundColor: isHovered ? project.color : "rgba(0,0,0,0)" }}
+      className="h-[15rem] xs:col-span-12 sm:col-span-6 md:col-span-4 relative shadow-lg p-4 hover:shadow-2xl border border-neutral-100 dark:border-neutral-800 hover:border-transparent hover:text-white transition-all ease-in-out duration-500 md:first-of-type:rounded-s-xl  md:last-of-type:rounded-e-xl mb-[4rem]"
+      style={{
+        background: isHovered
+          ? `linear-gradient(to  left, ${hexRgb(project.color, {
+              alpha: 0.5,
+              format: "css",
+            })}, ${project.color})`
+          : "rgba(0,0,0,0)",
+      }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <h4 className="text-lg font-medium capitalize text-center">
-        {project.title}
-      </h4>
-      <p className="font-light text-center">{project.subtitle}</p>
-      <img src={project.bgImage.image} className="absolute -top-[10rem]" />
-      {/* <div className="flex items-center gap-4 justify-center flex-wrap mt-4">
-        {project.technologies.map((tech) => (
-          <div
-            key={tech}
-            className="px-2 py-1 text-sm rounded border border-neutral-500"
-          >
-            {tech}
-          </div>
-        ))}
-      </div> */}
-      <button
-        onClick={() => {
-          redirect(project.url);
-        }}
-        className="mx-auto h-10 w-10 text-lg flex items-center justify-center mt-4 rounded-full border border-neutral-500 hover:border-primary hover:text-white hover:bg-primary hover:-rotate-45 transition-all ease-in-out duration-500"
-      >
-        <BsArrowRight />
-      </button>
+      <img
+        src={project.bgImage.image}
+        className=" absolute right-0 bottom-0 h-[18rem] drop-shadow-2xl"
+      />
+      <div className="w-1/2 flex justify-between flex-col h-full">
+        <div>
+          <h4 className="text-lg font-medium capitaliz">{project.title}</h4>
+          <p className="font-light">{project.subtitle}</p>
+        </div>
+        <button
+          onClick={() => {
+            redirect(project.url);
+          }}
+          className="btn btn-primary btn-sm w-max"
+        >
+          View
+          <BsArrowRight />
+        </button>
+      </div>
     </div>
   );
 };
