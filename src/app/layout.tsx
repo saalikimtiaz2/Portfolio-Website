1fd26c3c4fb5bd84759e6053ad1a21ab347e2f54
Salik Imtiaz <r.salikimtiaz@gmail.com>
2023-11-14 09:11:52 +0400

    updated head title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>Salik Imtiaz | Frontend Engineer</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
